53a874940f27c99215393fa41bb12ea598a7264c
ChrisWoodward73 <chris.woodward@bbc.co.uk>
2025-07-26 20:13:07 +0000

    name added

--- src/Ch01/01_02b/CodeDemo.cpp
+++ src/Ch01/01_02b/CodeDemo.cpp
@@ -1,3 +1,16 @@
 // Learning C++ 
 // Exercise 01_02
-// Hello World, by Eduardo Corpeño 
+// Hello World, by Chris Woodward
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hi There!" << std::endl;
+    std::cout << std::endl << std:: endl;
+    std::cout << "This is the 2nd line" << std::endl;
+    std::cout << std::endl << std:: endl;
+    std::cout << "This is the 3rd line" << std::endl;
+    std::cout << std::endl << std:: endl;
+    return (0);
+}
+
