734cc87ebb218189e5556e2b245106ef62b07034
ChrisWoodward73 <chris.woodward@bbc.co.uk>
2025-07-26 20:35:43 +0000

    Challenge completed

--- src/Ch01/01_03/CodeDemo.cpp
+++ src/Ch01/01_03/CodeDemo.cpp
@@ -1,15 +1,16 @@
 // Learning C++ 
 // Challenge 01_03
 // Console Interaction, by Eduardo Corpeño 
+// completed on 26th July 2025
 
 #include <iostream>
 #include <string>
 
 int main(){
     std::string str;
+    std::cout << "Hello, please enter your first name? " << std::flush;
     std::cin >> str;
-    std::cout << str;
-
-    std::cout << std::endl << std::endl;
+    std::cout << "Thank you " << str << std::endl;
+    std::cout << std::endl << std:: endl;
     return (0);
 }
